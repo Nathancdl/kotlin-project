8fcca596580f370820826598796c5f82b9e3e59c
alexmi94 <alexandrecance94@gmail.com>
2022-12-18 22:18:00 +0100

    Les 2 premières pages sont faites

--- app/src/main/java/com/example/kotlin_project/ConnectionLoginActivity.kt
+++ app/src/main/java/com/example/kotlin_project/ConnectionLoginActivity.kt
@@ -3,9 +3,9 @@ package com.example.kotlin_project
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SecondActivity : AppCompatActivity() {
+class ConnectionLoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        setContentView(R.layout.connection_login)
     }
 }
--- app/src/main/java/com/example/kotlin_project/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_project/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.kotlin_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch{
+            nextscreen()
+        }
+
+    }
+
+    suspend fun nextscreen(){
+        //function async excute after launch first layout
+        delay(2000)
+        val intent = Intent(this, ConnectionLoginActivity::class.java)
+        startActivity(intent);
+
     }
 }
