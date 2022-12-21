2ae474bfb6d6f2cddf484f32bf3940af1d4d54a0
Elias Merhrioui <e.merhrioui@gmail.com>
2022-12-21 20:35:12 +0100

    ui casi fini

--- app/src/main/java/com/example/kotlin_project/MyProfile.kt
+++ app/src/main/java/com/example/kotlin_project/MyProfile.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 
-class Profile : AppCompatActivity()  {
+class MyProfile : AppCompatActivity()  {
     override fun  onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.profile)
