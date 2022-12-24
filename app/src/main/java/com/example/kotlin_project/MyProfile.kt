0ba384e20358ea3009df2cef15dff8e9f11117af
Elias Merhrioui <e.merhrioui@gmail.com>
2022-12-24 02:53:13 +0100

    ui my profile done + commentaire

--- app/src/main/java/com/example/kotlin_project/MyProfile.kt
+++ app/src/main/java/com/example/kotlin_project/MyProfile.kt
@@ -7,6 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MyProfile : AppCompatActivity()  {
     override fun  onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.profile)
+        setContentView(R.layout.myprofile)
     }
 }
