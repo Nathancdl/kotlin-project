f5cd1bafe859580c8a908a4262208d3595263011
Nathan Caudeli <nathan.caudeli@hotmail.com>
2023-01-10 11:29:48 +0100

    Revert "error unresolved reference"
    This reverts commit 499ccc87aeacb0858d7869e9d5a70b5e0fd74dcc.

---
+++ app/src/main/java/com/example/kotlin_project/MyProfile.kt
@@ -0,0 +1,12 @@
+package com.example.kotlin_project
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MyProfile : AppCompatActivity()  {
+    override fun  onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.myprofile)
+    }
+}
