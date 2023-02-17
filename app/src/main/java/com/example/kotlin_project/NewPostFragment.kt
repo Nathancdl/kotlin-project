a15e5e70273909cb0a376e5eac7486077701fadf
alexmi94 <alexandrecance94@gmail.com>
2023-02-17 13:58:49 +0100

    Layout New Post

--- app/src/main/java/com/example/kotlin_project/NewPostFragment.kt
+++ app/src/main/java/com/example/kotlin_project/NewPostFragment.kt
@@ -1,7 +1,35 @@
 package com.example.kotlin_project
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ScrollView
 import androidx.fragment.app.Fragment
 
 class NewPostFragment : Fragment(R.layout.fragment_newpost){
-    
+    @SuppressLint("MissingInflatedId")
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        val rootView = inflater.inflate(R.layout.fragment_newpost, container, false)
+
+        val button_submit : Button = rootView.findViewById<Button>(R.id.newpostsubmit)
+
+        button_submit.setOnClickListener{
+            this.SubmitPost()
+        }
+
+        return rootView
+    }
+
+    private fun SubmitPost() {
+        // Here for submit post data
+    }
 }
